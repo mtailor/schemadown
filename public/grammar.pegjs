c79aa48a4100afd55c908ff700df9575b57f8987
Mtailor <emmanuel.letallieur@gmail.com>
2014-08-27 12:48:40 +0000

    suite

---
+++ public/grammar.pegjs
@@ -0,0 +1,16 @@
+
+
+start
+  = line
+
+line "line"
+  = left:identifier s arrow s right:identifier { return { left : left, right : right };}
+
+identifier "identifier"
+  = chars:[a-zA-Z]+ { return chars.join(""); }
+
+arrow "arrow"
+  = "=>"
+
+s "spaces"
+ = " "+
--- public/schemadown.js
+++ public/schemadown.js
@@ -1,13 +1,13 @@
 (function(){
 
-
     function newGraph(width, height, nodes, links, color){
 
         //some kind of factor indicating how much room is available
         var n = nodes.length;
-        var factor = (width + height) / Math.log(n);
-        var r = factor * 0.08;
-        console.log(factor)
+        var minSize = Math.min(width, height);
+        var factor = minSize / Math.log(n);
+        var charge = -0.15 * minSize * minSize / n;
+        var r      = 0.2 * minSize / Math.sqrt(n);
 
         //place the nodes on some kind of zigzag
         nodes.forEach(function(d, i) {
@@ -16,10 +16,7 @@
         });
 
         var force = d3.layout.force()
-            .charge(-factor * 20)
-            //.chargeDistance(factor * 0.5)
-            .gravity(200 / factor)
-            //.linkDistance(factor * 0.7)
+            .charge(charge)
             .size([width, height]);
 
         var svg = d3
@@ -28,9 +25,8 @@
             .attr("style", "border-color:" + color)
             .attr("width", width)
             .attr("height", height);
-
-
-
+            
+        
         force
             .nodes(nodes)
             .links(links)
@@ -49,7 +45,16 @@
             .enter()
             .append("circle")
             .attr("class", "node")
-            .attr("r", r);
+            .attr("r", r)
+            .call(force.drag)
+            ;
+
+        var cpt = { val : 0 };
+        addText(cpt, svg, "minSize : " + minSize)
+        addText(cpt, svg, "n : " + n)
+        addText(cpt, svg, "Math.log(n) : " + Math.log(n))
+        addText(cpt, svg, "charge : " + charge)
+        addText(cpt, svg, "r : " + r)
 
         force.on("tick", function() {
             link
@@ -59,7 +64,7 @@
                 .attr("y2", function(d) { return d.target.y; });
 
             node
-                .attr("cx", function(d) { return d.x = Math.max(r, Math.min(width - r, d.x)); })
+                .attr("cx", function(d) { return d.x = Math.max(r, Math.min(width  - r, d.x)); })
                 .attr("cy", function(d) { return d.y = Math.max(r, Math.min(height - r, d.y)); });
         });
     }
@@ -69,6 +74,17 @@
             source : n1,
             target : n2
         };
+    }
+    
+    function addText(cptHolder, root, text){
+        root
+            .append("text")
+            .text(text)
+            .attr("x", "50")
+            .attr("y", "" + (50 + (cptHolder.val * 15)))
+            .attr("font-size", "10px")
+            .attr("fill", "white");
+        cptHolder.val = cptHolder.val + 1;
     }
 
     (function(){
@@ -98,7 +114,36 @@
             newLink(g, f),
             newLink(g, d)
         ];
-        newGraph(250, 250, nodes, links, "blue");
+        newGraph(200, 200, nodes, links, "blue");
+    })();
+    (function(){
+        var a = {};
+        var b = {};
+        var c = {};
+        var d = {};
+        var e = {};
+        var f = {};
+        var g = {};
+        var h = {};
+        var i = {};
+        var j = {};
+        var nodes = [a, b, c, d, e, f, g, h, i, j];
+        var links = [
+            newLink(a, b),
+            newLink(b, c),
+            newLink(c, d),
+            newLink(d, e),
+            newLink(e, f),
+            newLink(f, a),
+            newLink(a, h),
+            newLink(a, i),
+            newLink(a, j),
+            newLink(g, a),
+            newLink(g, b),
+            newLink(g, f),
+            newLink(g, d)
+        ];
+        newGraph(1000, 1000, nodes, links, "gray");
     })();
     (function(){
         var a = {};
@@ -117,7 +162,7 @@
             newLink(f, d),
             newLink(f, e)
         ];
-        newGraph(800, 500, nodes, links, "green");
+        newGraph(400, 400, nodes, links, "green");
     })();
     (function(){
         var a = {};
@@ -130,7 +175,7 @@
             newLink(b, c),
             newLink(c, d)
         ];
-        newGraph(400, 800, nodes, links, "yellow");
+        newGraph(170, 170, nodes, links, "yellow");
     })();
     (function(){
         var a = {};
@@ -141,6 +186,17 @@
             newLink(a, b),
             newLink(b, c)
         ];
-        newGraph(150, 100, nodes, links, "orange");
+        newGraph(75, 75, nodes, links, "red");
+    })();
+    (function(){
+        var a = {};
+        var b = {};
+        var c = {};
+        var nodes = [a, b, c];
+        var links = [
+            newLink(a, b),
+            newLink(b, c)
+        ];
+        newGraph(750, 750, nodes, links, "purple");
     })();
 })();
