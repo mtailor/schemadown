d4be1117dcf50ffd7f472b024c9175467e449c73
Mtailor <emmanuel.letallieur@gmail.com>
2014-09-02 13:28:36 +0000

    suite

--- public/app.js
+++ public/app.js
@@ -1,46 +1,81 @@
-(function(){
+'use strict';
 
-    function newGraph(width, height, nodes, links, color){
 
-        //some kind of factor indicating how much room is available
+angular
+.module('myApp', [])
+.controller('mainController', function($scope, $http, parsingService, drawingService){
+    $http.get('example.schemadown').success(function(data){
+        $scope.input = data;
+    });
+    $scope.parseInput = function(){
+        parsingService
+            .parseAsNodesAndLinks($scope.input)
+            .then(function(res){
+                console.log(res);
+                drawingService.draw(res);
+            });
+    };
+})
+.service('parsingService', function($http){
+    this.parseAsNodesAndLinks = function(str) {
+        return $http
+            .get('grammar.pegjs')
+            .then(function(response){
+                var grammar = response.data;
+                return PEG.buildParser(grammar).parse(str);
+            });
+    }
+})
+.service('drawingService', function(){
+    this.draw = function(nodesAndLinks){
+        console.log("drawing");
+        var width = 500;
+        var height = 500;
+        var nodes = nodesAndLinks.nodes;
+        var links = nodesAndLinks.links;
+    
         var n = nodes.length;
         var minSize = Math.min(width, height);
-        var factor = minSize / Math.log(n);
         var charge = -0.15 * minSize * minSize / n;
         var r      = 0.2 * minSize / Math.sqrt(n);
-
+        
         //place the nodes on some kind of zigzag
         nodes.forEach(function(d, i) {
             d.x = n == 1 ? width / 2 : i * width / (n-1);
             d.y = i % 2 == 0 ? height : 0;
         });
-
-        var force = d3.layout.force()
+        
+        var svg = d3.select('svg');
+        
+        //clear
+        svg
+            .attr("width", width)
+            .attr("height", height)
+            .select("*")
+            .remove();
+            
+        //setup the layout    
+        var force = 
+            d3
+            .layout
+            .force()
             .charge(charge)
             .size([width, height]);
-
-        var svg = d3
-            .select("body")
-            .append("svg")
-            .attr("style", "border-color:" + color)
-            .attr("width", width)
-            .attr("height", height);
             
-        
+        //start it    
         force
             .nodes(nodes)
             .links(links)
             .start();
-
-        var link = svg
+            
+        var links = svg
             .selectAll(".link")
             .data(links)
             .enter()
             .append("line")
             .attr("class", "link");
-
-            
-       var gNodes = svg
+        
+        var gNodes = svg
             .selectAll("g.gnode")
             .data(nodes)
             .enter()
@@ -64,9 +99,7 @@
             .attr("y", -r )
             .attr("stroke", "none")
             .attr("stroke-opacity", 0.5)
-            .attr("fill", "none")
-            ;
-            
+            .attr("fill", "none");
             
         gNodes
             .append("text")
@@ -77,43 +110,23 @@
             .attr("x", -r*0.9)
             .attr("y", 3)
             .text(function(d){return d.name;});
-
-
-        force.on("tick", function() {
             
+        force.on("tick", function() {
             gNodes
                 .attr("transform", function(d) { 
                     d.x = Math.max(r, Math.min(width  - r, d.x));    
                     d.y = Math.max(r, Math.min(height - r, d.y));    
                     return 'translate(' + [d.x, d.y] + ')'; 
                 }
-            );  
-            
-            
-            link
+            );
+            links
                 .attr("x1", function(d) { return d.source.x; })
                 .attr("y1", function(d) { return d.source.y; })
                 .attr("x2", function(d) { return d.target.x; })
                 .attr("y2", function(d) { return d.target.y; });
         });
-    }
-
-    function newLink(n1, n2) {
-        return {
-            source : n1,
-            target : n2
-        };
-    }
+    };
     
     
-    function schemadown(file){
-        d3.text(file, function(content){
-           var nodesAndLinks = GeneratedParser.parse(content);
-           newGraph(500, 500, nodesAndLinks.nodes, nodesAndLinks.links, "black");
-        });
-    }
-    
-
-    schemadown("example.schemadown");
-
-})();
+})
+;
